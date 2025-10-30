36d8a1f4470b264d3e780213309e6bcfc782398d
pyot <petaboss2000@gmail.com>
2025-10-30 19:54:48 +0300

    Починил config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
 	apiBaseURL: "https://localhost:5001",
 	loginEndpoint: "login",
 	registration: "register",
-	videoWS: "ws://localhost:8000/videoWS"
+	videoWS: "ws://localhost:8000/websocket/connect_user"
 }
 
 export default config
